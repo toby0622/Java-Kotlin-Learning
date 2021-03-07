c69de4ab8afb85b8578e6b6b0ba7d3f9722ec2fc
Chi-Wen Teng <52705034+toby0622@users.noreply.github.com>
2021-03-07 21:11:17 +0800

    First Project Revise

--- Project-N01/src/main/java/com/toby/HelloWorld.java
+++ Project-N01/src/main/java/com/toby/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
     // psvm + tab = main function template
     public static void main(String[] args) {
         // sout + tab = println function template
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+
+        Person p = new Person();
+        p.hello();
     }
 }
--- Project-N01/src/main/kotlin/com/toby/HelloWorld.kt
+++ Project-N01/src/main/kotlin/com/toby/HelloWorld.kt
@@ -2,5 +2,16 @@ package com.toby
 
 // main + tab = main function template
 fun main() {
-    println("Hello Kotlin World!")
+    // println("Hello Kotlin World!")
+
+    // Human().hello()
+
+    val h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello() {
+        println("Hello Kotlin World!")
+    }
 }

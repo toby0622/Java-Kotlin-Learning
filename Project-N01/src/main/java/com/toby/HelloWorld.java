c892748862e6896ee49cd916dc11a1c7a80f328b
Chi-Wen Teng <52705034+toby0622@users.noreply.github.com>
2021-03-08 10:00:58 +0800

    First Project Revise

--- Project-N01/src/main/java/com/toby/HelloWorld.java
+++ Project-N01/src/main/java/com/toby/HelloWorld.java
@@ -8,5 +8,24 @@ public class HelloWorld {
 
         Person p = new Person();
         p.hello();
+
+        // Basic Variable -> No Method (Lower-case Letter)
+        int age = 20;
+        char c = '我';
+        byte b = 120;
+        float weight = 80.0f;
+        double height = 1.8;
+        boolean adult = true;
+        boolean enroll = false;
+
+        // Reference Varialbe -> With Method (Capital Letter)
+        String name = "Toby";
+        Integer age2 = 20;
+        Character c2 = '我';
+        Byte b2 = 120;
+        Float weight2 = 80.0f;
+        Double height2 = 1.8;
+        Boolean adult2 = true;
+        Boolean enroll2 = false;
     }
 }
